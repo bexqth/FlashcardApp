c88ff640fa19a1a5d1e1960aa9323c6a8430ae6d
Terézia Vrábeľová <vrabelova2@stud.uniza.sk>
2026-01-31 13:53:13 +0100

    - menu options hide/show

--- src/FlashcardApp.Desktop/ViewModels/MainWindowViewModel.cs
+++ src/FlashcardApp.Desktop/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,31 @@
-﻿namespace FlashcardApp.Desktop.ViewModels;
+﻿using System;
+using System.Linq.Expressions;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+namespace FlashcardApp.Desktop.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
+    [ObservableProperty]
+    private bool _sidePanelShow = true;
+
+    [ObservableProperty]
+    private ViewModelBase? _currentPage;
+
     public string Greeting { get; } = "Welcome to Avalonia!";
+
+    [RelayCommand]
+    public void SideButtonPressed()
+    {
+        Console.WriteLine(SidePanelShow);
+        if(SidePanelShow)
+        {
+            SidePanelShow = false;
+        }
+        else
+        {
+            SidePanelShow = true;
+        }
+    }
 }
