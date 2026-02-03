c9c34e01b1c0d691ca85068a8a5bd6890bf382fc
Terézia Vrábeľová <vrabelova2@stud.uniza.sk>
2026-02-03 21:33:31 +0100

    - feat: mask for class folder

---
+++ src/FlashcardApp.Desktop/ViewModels/ClassFolderViewModel.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using FlashcardApp.Core.Models;
+using FlashcardApp.Core.Services;
+using FlashcardApp.Desktop.ViewModels;
+
+namespace FlashcardApp.Desktop.ViewModels;
+
+public partial class ClassFolderViewModel : ViewModelBase
+{
+    public ClassFolderViewModel()
+    {
+
+    }
+
+}
